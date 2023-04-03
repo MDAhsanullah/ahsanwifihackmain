169f95e13cdfed1bba3b41c8896ff2c274b24ce3
MDAhsanullah <87916111+MDAhsanullah@users.noreply.github.com>
2023-04-03 16:22:31 +0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,13 +1,13 @@
 import os
-print('''\033[1;36;40mWifi_Hack Installer By THBD
+print('''\033[1;36;40mWifi_Hack Installer By Ahsanullah
 Your Device Must Be Rooted
 If Any Question,
-Contact Me On Telegram
-Tg_User:@biri_baba \n''')
+Contact Me On 014033232... i'm kidding
+FB:Sorry boro FB Nai \n''')
 os.system("pkg install -y root-repo")
 os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
-os.system("cd ..;git clone https://github.com/Mahfuz-THBD/Wifi_Hack")
+os.system("cd ..;git clone https://github.com/MDAhsanullah/ahsanwifihack")
 
-os.system("cd ..;chmod +x Wifi_Hack/birihack.py")
+os.system("cd ..;chmod +x ahsanwifihack/ahsanwifihack.py")
 
-print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python Wifi_Hack/birihack.py -i wlan0 -K")
+print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python ahsanwifihack/ahsanwifihack.py -i wlan0 -K")
